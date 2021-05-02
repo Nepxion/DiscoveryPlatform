ae9b32ab1984e69b5dbb65cb81745f26999f7b67
HaojunRen <1394997@qq.com>
2021-05-02 23:16:26 +0800

    Initialize platform projects

---
+++ discovery-platform-application/src/main/java/com/nepxion/discovery/platform/application/PlatformApplication.java
@@ -0,0 +1,24 @@
+package com.nepxion.discovery.platform.application;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Ning Zhang
+ * @version 1.0
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableTransactionManagement(proxyTargetClass = true)
+public class PlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformApplication.class, args);
+    }
+}
---
+++ version.bat
@@ -0,0 +1,19 @@
+@echo on
+@echo =============================================================
+@echo $                                                           $
+@echo $                     Nepxion Discovery                     $
+@echo $                                                           $
+@echo $                                                           $
+@echo $                                                           $
+@echo $  Nepxion Studio All Right Reserved                        $
+@echo $  Copyright (C) 2017-2050                                  $
+@echo $                                                           $
+@echo =============================================================
+@echo.
+@echo off
+
+@title Nepxion Discovery
+@color 0a
+
+call mvn versions:set -DgenerateBackupPoms=false -DnewVersion=1.0.0-SNAPSHOT
+pause

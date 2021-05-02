1cd8d1c20d72e7b5bb77c94f60df39e361d7cfd3
HaojunRen <1394997@qq.com>
2021-05-03 00:08:15 +0800

    Add platform banner

---
+++ discovery-platform-starter-server/src/main/java/com/nepxion/discovery/platform/server/constant/PlatformConstant.java
@@ -0,0 +1,14 @@
+package com.nepxion.discovery.platform.server.constant;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+public class PlatformConstant {
+    public static final String PLATFORM_VERSION = "1.0.0-SNAPSHOT";
+}

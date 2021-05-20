89d670038db1fef8eb590fa3a9155aca2705db7b
HaojunRen <1394997@qq.com>
2021-05-20 17:35:59 +0800

    Add ROLLBACK state to StateMachine

--- discovery-platform-starter-server/src/main/java/com/nepxion/discovery/platform/server/state/enums/Events.java
+++ discovery-platform-starter-server/src/main/java/com/nepxion/discovery/platform/server/state/enums/Events.java
@@ -11,13 +11,54 @@ package com.nepxion.discovery.platform.server.state.enums;
 
 public enum Events {
     // 事件：执行增加
-    EVENT_DO_ADD,
+    EVENT_DO_ADD(20, "EVENT_DO_ADD"),
     // 事件：执行修改
-    EVENT_DO_MODIFY,
+    EVENT_DO_MODIFY(21, "EVENT_DO_MODIFY"),
     // 事件：执行删除
-    EVENT_DO_DELETE,
+    EVENT_DO_DELETE(22, "EVENT_DO_DELETE"),
     // 事件：执行回滚
-    EVENT_DO_ROLLBACK,
+    EVENT_DO_ROLLBACK(23, "EVENT_DO_ROLLBACK"),
     // 事件：执行发布
-    EVENT_DO_PUBLISH
+    EVENT_DO_PUBLISH(24, "EVENT_DO_PUBLISH");
+
+    private int index;
+    private String value;
+
+    private Events(int index, String value) {
+        this.index = index;
+        this.value = value;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static Events fromString(int index) {
+        for (Events type : Events.values()) {
+            if (type.getIndex() == index) {
+                return type;
+            }
+        }
+
+        throw new IllegalArgumentException("No matched type with index=" + index);
+    }
+
+    public static Events fromString(String value) {
+        for (Events type : Events.values()) {
+            if (type.getValue().equalsIgnoreCase(value)) {
+                return type;
+            }
+        }
+
+        throw new IllegalArgumentException("No matched type with value=" + value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }   
 }
--- discovery-platform-starter-server/src/main/java/com/nepxion/discovery/platform/server/state/enums/States.java
+++ discovery-platform-starter-server/src/main/java/com/nepxion/discovery/platform/server/state/enums/States.java
@@ -11,13 +11,54 @@ package com.nepxion.discovery.platform.server.state.enums;
 
 public enum States {
     // 状态：初始
-    STATE_INITIAL,
+    STATE_INITIAL(10, "STATE_INITIAL"),
     // 状态：待增加
-    STATE_TO_ADD,
+    STATE_TO_ADD(11, "STATE_TO_ADD"),
     // 状态：待修改
-    STATE_TO_MODIFY,
+    STATE_TO_MODIFY(12, "STATE_TO_MODIFY"),
     // 状态：待删除
-    STATE_TO_DELETE,
+    STATE_TO_DELETE(13, "STATE_TO_DELETE"),
     // 状态：已发布
-    STATE_PUBLISHED
+    STATE_PUBLISHED(14, "STATE_PUBLISHED");
+
+    private int index;
+    private String value;
+
+    private States(int index, String value) {
+        this.index = index;
+        this.value = value;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static States fromString(int index) {
+        for (States type : States.values()) {
+            if (type.getIndex() == index) {
+                return type;
+            }
+        }
+
+        throw new IllegalArgumentException("No matched type with index=" + index);
+    }
+
+    public static States fromString(String value) {
+        for (States type : States.values()) {
+            if (type.getValue().equalsIgnoreCase(value)) {
+                return type;
+            }
+        }
+
+        throw new IllegalArgumentException("No matched type with value=" + value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }

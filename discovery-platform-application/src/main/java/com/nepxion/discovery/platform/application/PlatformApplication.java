1320bd818638340695ae3ce68c71671055305862
HaojunRen <1394997@qq.com>
2021-05-22 23:08:47 +0800

    Structured refactoring for DiscoveryPlatform

--- discovery-platform-application/src/main/java/com/nepxion/discovery/platform/application/PlatformApplication.java
+++ discovery-platform-application/src/main/java/com/nepxion/discovery/platform/application/PlatformApplication.java
@@ -5,17 +5,19 @@ package com.nepxion.discovery.platform.application;
  * <p>Description: Nepxion Discovery</p>
  * <p>Copyright: Copyright (c) 2017-2050</p>
  * <p>Company: Nepxion</p>
- *
  * @author Ning Zhang
  * @author Haojun Ren
  * @version 1.0
  */
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 import org.springframework.boot.SpringApplication;
 
 import com.nepxion.discovery.platform.server.annotation.EnablePlatformServer;
 
 @EnablePlatformServer
+@EnableAdminServer
 public class PlatformApplication {
     public static void main(String[] args) {
         SpringApplication.run(PlatformApplication.class, args);

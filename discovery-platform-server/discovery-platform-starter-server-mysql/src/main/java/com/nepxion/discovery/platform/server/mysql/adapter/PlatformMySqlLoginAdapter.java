e520441356d267e3102e41ba0196f922ea2a9504
HaojunRen <1394997@qq.com>
2021-05-29 03:59:31 +0800

    Coding style

--- discovery-platform-server/discovery-platform-starter-server-mysql/src/main/java/com/nepxion/discovery/platform/server/mysql/adapter/PlatformMySqlLoginAdapter.java
+++ discovery-platform-server/discovery-platform-starter-server-mysql/src/main/java/com/nepxion/discovery/platform/server/mysql/adapter/PlatformMySqlLoginAdapter.java
@@ -5,7 +5,6 @@ package com.nepxion.discovery.platform.server.mysql.adapter;
  * <p>Description: Nepxion Discovery</p>
  * <p>Copyright: Copyright (c) 2017-2050</p>
  * <p>Company: Nepxion</p>
- *
  * @author Haojun Ren
  * @version 1.0
  */

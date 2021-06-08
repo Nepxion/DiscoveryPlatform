0dad9d0a7ff6ca57afc26e2605b30845699e4db8
fuxiangyang <9t123456>
2021-06-08 14:43:24 +0800

    delete hutool reference

--- discovery-platform-server/discovery-platform-starter-server/src/main/java/com/nepxion/discovery/platform/server/tool/DateTool.java
+++ discovery-platform-server/discovery-platform-starter-server/src/main/java/com/nepxion/discovery/platform/server/tool/DateTool.java
@@ -17,8 +17,6 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
-import cn.hutool.core.date.DateUtil;
-
 import org.apache.commons.lang3.StringUtils;
 
 public class DateTool {
